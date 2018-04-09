7d5aeb320d7a4727635f7b2ec8e4c8ae29b82d6c
Tanner Fiscus <tanner@tannerfiscus.com>
2018-04-08 17:11:53 -0700

    Leave current date check to the client-side

--- src/components/hours/Hours.js
+++ src/components/hours/Hours.js
@@ -6,33 +6,49 @@ import React from 'react'
 import hours from '../../constants/hours'
 import './Hours.css'
 
-const Hours = ({ className }) => {
-    // Returns 0-6, so we can rely on index number
-    // since our hours constants start on Sunday
-    const currentDay = getDay(new Date())
-    
-    return (
-        <dl className={`hours ${className}`}>
-        {
-            hours.map(({ day, hourEnd, hourStart, isOpen }, i) => (
-                <div 
-                    className={classnames('hours-row', {
-                        'hours-row--current-day': currentDay === i,
-                    })}
-                    key={day}
-                >
-                    <dt className='hours-row__day'>
-                        { day }
-                    </dt>
-                    <dd className='hours-row__time'>
-                        { !isOpen && 'Closed' }
-                        { isOpen && `${hourStart} - ${hourEnd}` }
-                    </dd>
-                </div>
-            ))
-        }
-        </dl>
-    )
+class Hours extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = { currentDayIndex: null };
+    }
+
+    // Since this is a statically generated site, we
+    // can't rely on using a date on the server because
+    // it will just be the date of which the static site
+    // was generated. Let's leave this to the client.
+    componentDidMount() {
+        // Returns 0-6, so we can rely on index number
+        // since our hours constants start on Sunday
+        this.setState({
+            currentDayIndex: getDay(new Date()),
+        });
+    }
+
+    render() {
+        return (
+            <dl className={`hours ${this.props.className}`}>
+            {
+                hours.map(({ day, hourEnd, hourStart, isOpen }, i) => (
+                    <div
+                        className={classnames('hours-row', {
+                            'hours-row--current-day': this.state.currentDayIndex === i,
+                        })}
+                        key={day}
+                    >
+                        <dt className='hours-row__day'>
+                            { day }
+                        </dt>
+                        <dd className='hours-row__time'>
+                            { !isOpen && 'Closed' }
+                            { isOpen && `${hourStart} - ${hourEnd}` }
+                        </dd>
+                    </div>
+                ))
+            }
+            </dl>
+        )
+    }
+
 }
 
 Hours.defaultProps = {
