1bba00dbd8e51dcaef8e4d49da9eba5fef1845ab
Tanner Fiscus <tanner@tannerfiscus.com>
2021-09-29 17:23:00 -0400

    Remove outdated health notice

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,24 +19,6 @@ const TemplateWrapper = ({ children }) => (
     <Header />
     
     <div className='page-content'>
-
-      <div className="page-health-notice-wrapper">
-        <section className="page-health-notice">
-          <ContentWrapper>
-            <div className="health-notice">
-                <h3><i className="fa fa-info-circle" /> Reopening Information</h3>
-                <br />
-                <p>I will be reopening on <b>Tuesday, June 9, 2020.</b></p>
-                <br/>
-                <p>As there is a lot of catching up to do, please be sure to call ahead and book an appointment.</p>
-                <p>I will try to get back to you all in a timely manner. Please bear with me and be patient as we all get back on track.</p>
-                <p>Can't wait to see you all,</p>
-                <p>Patti Fiscus<br/>TNT Cuts</p>
-            </div>
-          </ContentWrapper>
-        </section>
-      </div>
-
       {children()}
     </div>
     
